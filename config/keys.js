4e6264f9d6752cee1524c4a407138ee6db259adc
Dilly Sanborn-Marsh <csanbornmarsh@gmail.com>
2023-11-03 14:35:51 -0400

    took out keys.js from gitignore

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientSecret: "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",
+  GoogleClientID:
+    "205254738044-4di1c238qh0vicpdrhov5ttam9jlnhek.apps.googleusercontent.com",
+  mongoURI:
+    "mongodb+srv://csanbornmarsh:8C6cxVgBAASiVj8U@dev-emaily.pwhr9rd.mongodb.net/",
+  mongoURIProd:
+    "mongodb+srv://csanbornmarsh:HNq2v2JsDZKCI1zs@emaily-0.rqlnjbu.mongodb.net/",
+  cookieKey: "v;nfadkjnv;kefngkj;nfdkgnfdng",
+};
